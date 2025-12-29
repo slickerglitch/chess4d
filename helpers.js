2b3943d2f7190a6503c76f03dd4a994ef885b9f0
JK <slickerglitch@gmail.com>
2025-12-29 14:48:29 -0600

    Corrected typo in arrow structure of Fxn: isInteger

--- helpers.js
+++ helpers.js
@@ -1,22 +1,22 @@
-const isString = s => typeof s === 'string'; 
+const isString = s => typeof s === "string";
 
-const isFxn = f => typeof f === 'function';
+const isFunction = f => typeof f === "function";
 
-const isInteger = n => typeof n === 'number' && isFinite(n) && Math.floor(n) === n; 
+const isInteger = n => typeof n === "number" && isFinite(n) && Math.floor(n) === n;
 
 const validAnimationSpeed = speed => {
-    if (!isInteger(speed)) { 
-      return false;
-    }
-    
-    return speed === 'fast' || speed === 'slow' ? true : speed >= 0; 
+	if (!isInteger(speed)) {
+		return false;
+	}
+
+	return speed === "fast" || speed === "slow" ? true : speed >= 0;
 };
 
-const validThrottleRate = rate => isInteger(rate) && rate >= 1; 
+const validThrottleRate = rate => isInteger(rate) && rate >= 1;
 
 const validMove = move => {
-   let squares = move.split('-')
-   return !isString(move) || squares.length !== 2 ? false : validSquare(squares[0]) && validSquare(squares[1]);
-}; 
+	let squares = move.split("-");
+	return !isString(move) || squares.length !== 2 ? false : validSquare(squares[0]) && validSquare(squares[1]);
+};
 
 const validSquare = square => isString(square) && square.search(/^[a-h][1-8]$/) !== -1;
