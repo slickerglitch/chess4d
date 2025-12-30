219ab25e381b7b2df4563e3677beea63ff7aa161
JK <slickerglitch@gmail.com>
2025-12-30 01:05:36 -0600

    Small adjustments to helper fxns

--- helpers.js
+++ helpers.js
@@ -4,19 +4,19 @@ const isFunction = f => typeof f === "function";
 
 const isInteger = n => typeof n === "number" && isFinite(n) && Math.floor(n) === n;
 
-const validAnimationSpeed = speed => {
-	if (!isInteger(speed)) {
-		return false;
-	}
-
-	return speed === "fast" || speed === "slow" ? true : speed >= 0;
-};
-
 const validThrottleRate = rate => isInteger(rate) && rate >= 1;
+
+const validSquare = square => isString(square) && square.search(/^[a-h][1-8]$/) !== -1;
 
 const validMove = move => {
 	let squares = move.split("-");
 	return !isString(move) || squares.length !== 2 ? false : validSquare(squares[0]) && validSquare(squares[1]);
 };
 
-const validSquare = square => isString(square) && square.search(/^[a-h][1-8]$/) !== -1;
+const validAnimationSpeed = speed => {
+	if (!isInteger(speed)) {
+		return false;
+	}
+	return speed === "fast" || speed === "slow" ? true : speed >= 0;
+};
+
